78513fc68c84eae6c0a24bdc6a1bb3c04405afc1
Jan Wennrich <jan@pcsg.de>
2018-02-19 13:41:55 +0100

    feat: added mysql step

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,42 +1,40 @@
 #!/usr/bin/env bash
 
-# Import vagrant-shell-scripts
-#SCRIPT_DIR="$(dirname "$0")"
-#"$SCRIPT_DIR/vagrant-shell-scripts/ubuntu.sh"
-
-update_complete_file = "/etc/passdora_apt_complete"
-
-QUIQQER_DB_NAME = "quiqqer"
-QUIQQER_DB_USER_NAME = "quiqqer"
-QUIQQER_DB_USER_PW = "quiqqer"
-
-if [! -f "$update_complete_file"]; then
-
-    # Check if device is online
-    wget -q --spider https://update.quiqqer.com
-
-    if [ $? -eq 1 ]; then
-        echo "You need an internet connection to run the script."
-        exit
-    fi
-
-    sudo apt update -y
-    sudo apt full-upgrade -y
-    sudo apt install php nginx mysql php-curl php-dom php-mbstring php-xml php-zip php-imagick php-gd php-mysql php-bcmath php-dev libsodium-dev php-libsodium -y
-    
-    sudo touch "$update_complete_file"
-    
-    # Necessary?
-    sudo shutdown now -r
-fi
-
+QUIQQER_DB_NAME="quiqqer"
+QUIQQER_DB_USER_NAME="quiqqer"
+QUIQQER_DB_USER_PW="quiqqer"
 
 # Todo: set MySQL root-User PW?
 
 
-# Create QUIQQER DB & User and grant him access to the DB
-sql_query = "CREATE DATABASE $QUIQQER_DB_NAME; CREATE USER '$QUIQQER_DB_USER_NAME'@'localhost' IDENTIFIED BY '$QUIQQER_DB_USER_PW'; GRANT ALL ON $QUIQQER_DB_NAME.* TO '$QUIQQER_DB_USER_NAME'@'localhost';"
+function mysql_Echo() {
+    echo -e "\033[0;32mmysql: $1\033[0m"
+}
 
-mysql -u "root" -e "$sql_query";
 
+function mysql_CreateDB() {
+    sudo mysql -e "CREATE DATABASE $QUIQQER_DB_NAME;";
+}
+
+
+function mysql_CreateUser() {
+    sudo mysql -e "CREATE USER '$QUIQQER_DB_USER_NAME'@'localhost' IDENTIFIED BY '$QUIQQER_DB_USER_PW';";
+}
+
+
+function mysql_GrantPermissions() {
+    sudo mysql -e "GRANT ALL ON $QUIQQER_DB_NAME.* TO '$QUIQQER_DB_USER_NAME'@'localhost';";
+}
+
+
+function mysql_ExecuteStep() {
+    mysql_Echo "Creating QUIQQER DB..."
+    mysql_CreateDB
+    
+    mysql_Echo "Creating QUIQQER DB-User..."
+    mysql_CreateUser
+    
+    mysql_Echo "Granting QUIQQER DB-User access to QUIQQER DB..."
+    mysql_GrantPermissions
+}
 
