653beb3282b39a4eca011f4c94a93cffc6350c98
Jan Wennrich <jan@pcsg.de>
2018-02-20 13:16:16 +0100

    feat: cancel script on error

--- setup_image.sh
+++ setup_image.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 . scripts/apt.sh
 . scripts/mysql.sh
 . scripts/php.sh
@@ -7,13 +16,15 @@
 . scripts/nginx.sh
 . scripts/system.sh
 
-echo -e "\033[0;32mapt: Starting setup...\033[0m"
+echo -e "\033[0;32mStarting setup...\033[0m"
 
 #apt_ExecuteStep
-#mysql_ExecuteStep
+mysql_ExecuteStep
 #php_ExecuteStep
-#quiqqer_ExecuteStep
+quiqqer_ExecuteStep
 #nginx_ExecuteStep
-system_ExecuteStep
+#system_ExecuteStep
 
-echo -e "\033[0;32mapt: Setup completed.\033[0m"
+echo -e "\033[0;32mSetup completed.\033[0m"
+
+exit 0
