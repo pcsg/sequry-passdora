d71c850474ce88100afe8ebc4286bb40ad2cab93
Jan Wennrich <jan@pcsg.de>
2018-02-20 12:02:46 +0100

    feat: added setup started and completed message

--- setup_image.sh
+++ setup_image.sh
@@ -7,9 +7,13 @@
 . scripts/nginx.sh
 . scripts/system.sh
 
+echo -e "\033[0;32mapt: Starting setup...\033[0m"
+
 #apt_ExecuteStep
 #mysql_ExecuteStep
 #php_ExecuteStep
 #quiqqer_ExecuteStep
 #nginx_ExecuteStep
 system_ExecuteStep
+
+echo -e "\033[0;32mapt: Setup completed.\033[0m"
