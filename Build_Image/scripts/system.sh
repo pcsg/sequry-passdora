b99e733af2fef070bea91bfa337bfce63317c463
Jan Wennrich <jan@pcsg.de>
2018-02-21 10:43:51 +0100

    refactor: made gpu and hostname functions having parameters

--- Build_Image/scripts/system.sh
+++ Build_Image/scripts/system.sh
@@ -13,23 +13,23 @@ function system_Reboot() {
 
 
 # Changes the hostname to passdora
-function system_ChangeHostname() {
+function system_SetHostname() {
     echo "passdora" | sudo tee /etc/hostname > /dev/null
-    sudo sed -i "s/127\.0\.1\.1.*raspberrypi/127.0.1.1 passdora/g" /etc/hosts
+    sudo sed -i "s/127\.0\.1\.1.*raspberrypi/127.0.1.1 $1/g" /etc/hosts
 }
 
 
 # Reduces the memory the GPU can use to 16 (minimum)
-function system_ReduceGpuMem() {
-    echo "gpu_mem=16" | sudo tee --append /boot/config.txt > /dev/null
+function system_SetGpuMem() {
+    echo "gpu_mem=$1" | sudo tee --append /boot/config.txt > /dev/null
 }
 
 
 # Executes the system setup steps in correct order
 function system_ExecuteStep() {
     system_Echo "Changing hostname..."
-    system_ChangeHostname
+    system_SetHostname "passdora"
     
     system_Echo "Reducing GPU memory..."
-    system_ReduceGpuMem
+    system_SetGpuMem 16
 }
