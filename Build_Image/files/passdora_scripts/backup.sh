fa8bf7776b4aca28a7b5acf834b99d2fa2b11641
Jan Wennrich <jan@pcsg.de>
2018-03-07 12:06:20 +0100

    feat: also backup system /etc folder (#7)

--- Build_Image/files/passdora_scripts/backup.sh
+++ Build_Image/files/passdora_scripts/backup.sh
@@ -6,6 +6,7 @@
 # Backup includes:                                                    #
 #   - QUIQQER database                                                #
 #   - QUIQQER etc/ folder                                             #
+#   - System etc/ folder                                              #
 #                                                                     #
 # Author: Jan Wennrich (PCSG)                                         #
 #                                                                     #
@@ -35,6 +36,8 @@ RESTORE_KEY=$restore_key
 # Create backup folder
 echo "Creating backup folder..."
 mkdir -p ${TEMP_BACKUP_FOLDER}
+mkdir -p ${TEMP_BACKUP_FOLDER}/quiqqer_etc
+mkdir -p ${TEMP_BACKUP_FOLDER}/system_etc
 
 # TODO: copy backups directly into tar instead of placing them in a folder first
 
@@ -44,13 +47,17 @@ sudo mysqldump --databases quiqqer > ${TEMP_BACKUP_FOLDER}/database.sql
 
 # Copy QUIQQER etc/ folder
 echo "Copying QUIQQER etc/ folder..."
-sudo cp -r /var/www/html/etc ${TEMP_BACKUP_FOLDER}/
+sudo cp -r /var/www/html/etc/* ${TEMP_BACKUP_FOLDER}/quiqqer_etc
+
+# Copy system etc/ folder
+echo "Copying System etc/ folder..."
+sudo cp -r /etc/* ${TEMP_BACKUP_FOLDER}/system_etc
 
 # Tar everything in the temporary backup directory and then encrypt it
 (
     cd ${TEMP_BACKUP_FOLDER}
     echo "Packing everything into an encrypted archive..."
-    tar -cz * | gpg -c --batch --yes --passphrase ${RESTORE_KEY} -o ${ENCRYPTED_BACKUP_FILE}
+    sudo tar -cz * | gpg -c --batch --yes --passphrase ${RESTORE_KEY} -o ${ENCRYPTED_BACKUP_FILE}
 )
 
 # Remove temp folder
@@ -64,7 +71,9 @@ if mountpoint -q ${USB_MOUNTING_POINT}; then
     echo "Copying backup to USB-drive..."
 
     # Create the backup folder
-    mkdir ${USB_MOUNTING_POINT}/passdora-backups/
+    if [ ! -d  ${USB_MOUNTING_POINT}/passdora-backups/ ]; then
+        sudo mkdir ${USB_MOUNTING_POINT}/passdora-backups/
+    fi
 
     # Copy the backup
     sudo cp ${ENCRYPTED_BACKUP_FILE} ${USB_MOUNTING_POINT}/passdora-backups/
