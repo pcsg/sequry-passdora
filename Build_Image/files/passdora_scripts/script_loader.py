723caff8018af0bb35ae628a5972f46262691d4c
Jan Wennrich <jan@pcsg.de>
2018-03-23 16:49:30 +0100

    feat: added backup restore script (#8)

--- Build_Image/files/passdora_scripts/script_loader.py
+++ Build_Image/files/passdora_scripts/script_loader.py
@@ -1,11 +1,13 @@
 from lib.autostart.ShowIP import *
 from lib.autostart.BackupButtonListener import *
 from lib.autostart.ShutdownListener import *
+from lib.autostart.RestoreListener import *
 
 autostarts = [
-    ShowIP(),
-    BackupButtonListener(),
-    ShutdownListener()
+   ShowIP(),
+   BackupButtonListener(),
+   ShutdownListener(),
+   RestoreListener()
 ]
 
 for Autostart in autostarts:
