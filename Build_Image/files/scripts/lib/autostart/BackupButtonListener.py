7d4e32e3c1f615c55d140430cf35d463a369aebd
Jan Wennrich <jan@pcsg.de>
2018-04-20 12:18:14 +0200

    feat: BackupButtonListener now beeps when button is pressed long enough

--- Build_Image/files/scripts/lib/autostart/BackupButtonListener.py
+++ Build_Image/files/scripts/lib/autostart/BackupButtonListener.py
@@ -14,6 +14,7 @@ import subprocess
 
 from lib.autostart.AbstractAutostart import AbstractAutostart
 from lib.button.Button import Button
+from lib.buzzer.Buzzer import Buzzer
 from lib.display.Display import Display
 
 
@@ -22,17 +23,20 @@ class BackupButtonListener(AbstractAutostart):
     BUTTON_HOLD_TIME_MIN = 3
 
     # After how many seconds of button pressing the event should be ignored?
-    BUTTON_HOLD_TIME_MAX = 4
+    BUTTON_HOLD_TIME_MAX = 5
 
     # Instructions how to use this script
     MESSAGE_INSTRUCTIONS = "Press and hold the button for {0} seconds to create a backup.".format(BUTTON_HOLD_TIME_MIN)
 
     button = Button.get_instance()
 
+    buzzer = Buzzer.get_instance()
+
     display = Display.get_instance()
 
-    def __init__(self, *args, **kwargs):
-        super(BackupButtonListener, self).__init__(*args, **kwargs)
+    has_beeped = False
+
+    has_locked_display = False
 
     def run(self):
         self.button.add_release_listener(self.button_release_listener)
@@ -40,27 +44,27 @@ class BackupButtonListener(AbstractAutostart):
         self.button.add_hold_listener(self.button_hold_listener)
 
     def button_press_listener(self):
-        self.display.Lock.acquire()
+        self.has_beeped = False
+
+        if not self.display.Lock.locked():
+            self.display.Lock.acquire()
+            self.has_locked_display = True
 
         print("Button pressed. Hold for {0} seconds to create a backup...".format(self.BUTTON_HOLD_TIME_MIN))
         self.display.show("Hold button for", "3 seconds")
 
-        self.display.show_countdown(2, self.BUTTON_HOLD_TIME_MIN, "{0} seconds")
-
     def button_hold_listener(self, seconds: float):
-        if self.BUTTON_HOLD_TIME_MAX > seconds > self.BUTTON_HOLD_TIME_MIN:
-            self.display.hide_countdown()
+        if self.BUTTON_HOLD_TIME_MAX > seconds > self.BUTTON_HOLD_TIME_MIN and not self.has_beeped:
+            self.has_beeped = True
+
+            self.buzzer.beep()
 
             print("Release button to start backup")
             self.display.show("Release button to", "start backup")
 
     def button_release_listener(self, seconds: float):
-        self.display.hide_countdown()
-
         # Check if button was pressed long enough
         if self.BUTTON_HOLD_TIME_MAX > seconds > self.BUTTON_HOLD_TIME_MIN:
-            print('Creating backup... You may release the button now.')
-
             self.display.show("Creating Backup", "")
             self.display.show_loader(2)
 
@@ -73,5 +77,7 @@ class BackupButtonListener(AbstractAutostart):
         # Print that the button can be used again
         print(self.MESSAGE_INSTRUCTIONS)
 
-        self.display.Lock.release()
+        if self.has_locked_display:
+            self.display.Lock.release()
+
         self.display.show_default()
