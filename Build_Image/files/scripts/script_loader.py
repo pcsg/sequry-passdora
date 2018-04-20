08d263a952c93c80b2c8b87006668ed663eac0be
Jan Wennrich <jan@pcsg.de>
2018-04-20 13:05:39 +0200

    feat: added SSH listener to toggle SSH with the button (#9)

--- Build_Image/files/scripts/script_loader.py
+++ Build_Image/files/scripts/script_loader.py
@@ -2,16 +2,18 @@ from lib.autostart.ShowIP import *
 from lib.autostart.BackupButtonListener import *
 from lib.autostart.ShutdownListener import *
 from lib.autostart.RestoreListener import *
+from lib.autostart.SshListener import SshListener
 from lib.autostart.UpdateListener import *
 from lib.autostart.SetupListener import *
 
 autostarts = [
-   ShowIP(),
-   BackupButtonListener(),
-   ShutdownListener(),
-   RestoreListener(),
-   UpdateListener(),
-   SetupListener()
+    ShowIP(),
+    BackupButtonListener(),
+    ShutdownListener(),
+    RestoreListener(),
+    UpdateListener(),
+    SetupListener(),
+    SshListener()
 ]
 
 for Autostart in autostarts:
